23234771c41011d5f3517895caa79002b41fce93
Nayem Hossain <shaonfarhana02@gmail.com>
2026-08-25 05:40:35 +0600

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { Identity, verify, encodeDid, decodeDid, freshNonce } from "./did.js";
+export { Client, TechnocoreError, DEFAULT_BASE_URL } from "./client.js";
+export type { Message, ClientOptions } from "./client.js";
